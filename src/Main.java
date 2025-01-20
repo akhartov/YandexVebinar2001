0268d98f14a5998c30d9621cba30967c1c63ddd6
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2025-01-20 20:31:34 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+package ru.my;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int magicNumber = Math.abs(new Random().nextInt())%10;
+        System.out.println("Загадано число. Отгадайте его.");
+        System.out.println(magicNumber);
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 3; ++i) {
+            try {
+                System.out.println("Вводите число");
+                int myInt = s.nextInt();
+                if (myInt > magicNumber)
+                    System.out.println("Ваше число больше");
+                else if (myInt < magicNumber)
+                    System.out.println("Ваше число меньше");
+                else {
+                    System.out.println("Угадали");
+                    break;
+                }
+
+                System.out.println("Еще раз? 1/0");
+                int next = s.nextInt();
+                if (next == 1)
+                    continue;
+                break;
+            } catch (java.util.InputMismatchException e) {
+                s.next();
+                System.out.println(e);
+                System.out.println("Что-то не то ввели");
+            } catch (Exception e) {
+                System.out.println("Какая-то жесть творится!");
+            }
+        }
     }
 }
